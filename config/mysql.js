38086fe336b9f1ef95088787bc320579edc2b0b8
batrick-swaistan <batrickswaistan@gmail.com>
2024-09-11 09:28:31 +0530

    Redis

--- config/mysql.js
+++ config/mysql.js
@@ -3,14 +3,16 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const sequelize = new Sequelize("business_optima", "root", process.env.DB_PASSWORD, {
-  host: "localhost",
-  port: 3310,
-  dialect: "mysql",
-  logging: false,
-});
-
-
-
+const sequelize = new Sequelize(
+  "business_optima",
+  "batrick",
+  process.env.DB_PASSWORD,
+  {
+    host: "34.132.178.82",
+    port: 3306,
+    dialect: "mysql",
+    logging: false,
+  }
+);
 
 module.exports = sequelize;
--- config/redis.js
+++ config/redis.js
@@ -1,5 +1,13 @@
 const redis = require("redis");
-const client = redis.createClient();
+// const client = redis.createClient();
+
+const client = redis.createClient({
+  url: "rediss://red-crg2m3ij1k6c739bmru0:lQD8nefY6tsMDiFp7S5HxL1J2daKAW4R@oregon-redis.render.com:6379",
+  socket: {
+    tls: true, // Enable SSL/TLS
+    rejectUnauthorized: false, // Skip certificate verification (optional, but useful for cloud environments)
+  },
+});
 
 client
   .connect()
